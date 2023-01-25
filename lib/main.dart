8a5553ce145968202015e22592cc50f22f4b2981
ignac_borovy <i.borovy@gmail.com>
2023-01-25 18:53:11 +0100

    Init project with base structure

---
+++ lib/main.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+import 'package:it_book/src/my_app.dart';
+
+void main() {
+  runApp(const MyApp());
+}
---
+++ lib/src/screens/book_detail_screen.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class BookDetailScreen extends StatelessWidget {
+  const BookDetailScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
